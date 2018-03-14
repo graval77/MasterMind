4455a8528374efc223ba7a17593ed81a326a7d48
Gopesh Raval <graval77@gmail.com>
2018-03-14 10:39:01 -0400

    Update MasterMind.java

--- MasterMind.java
+++ MasterMind.java
@@ -1,28 +1,23 @@
 import java.util.*;
 import java.io.*;
-class MasterMind
-{
-	public static void main(String[] args)throws IOException
-	{
+class MasterMind{
+	public static void main(String[] args)throws IOException{
 		System.out.println("***Welcome to MasterMind with digits***");
 		GuessClass gc=new GuessClass();
 		gc.startGame();
 	}
 }
 
-class GuessClass
-{
+class GuessClass{
 	private ArrayList<String> genrtdNos=new ArrayList<String>();
 	int choice;
 	FeedbackGenerator result=new FeedbackGenerator();
 	FeedbackGenerator fdbk=new FeedbackGenerator();
-	public GuessClass()
-	{
+	public GuessClass(){
 		numGen();
 	}
 	
-	public void startGame()
-	{
+	public void startGame(){
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Menu:-");
 		System.out.println("1. About the Game");
@@ -30,8 +25,7 @@ class GuessClass
 		System.out.println("3. Start Game");
 		System.out.println("Enter your Choice:");
 		choice=sc.nextInt();
-		switch(choice)
-		{
+		switch(choice){
 			case 1:	
 					System.out.println("*********************************");
 					System.out.println("MasterMind is a code breaking game for two players. One player becomes the codemaker, the other the codebreaker.");
@@ -45,7 +39,7 @@ class GuessClass
 					System.out.println("*********************************");
 					System.out.println("We will be working with digital number 0-9. For each game, the program will guess an ordered four non-repeated digits with minimum number of guesses with feedback provided on how good the current guess is.");
 					System.out.println("Suppose the second player kept a number 2365 in his/her mind that the program needs to guess, which is unknown to the program.");
-					System.out.println("In the first round the program has no information, so it makes an initial guess, e.g., 1234. Then the FeedbackGenerator will provide a feedback 0A 2B, meaning that the program guessed two digits correctly but they are not in the correct position. So according to this information, in the second round the program will adjust the guess to “2135”, then the FeedbackGenerator will provide with a feedback “1A 2B”, meaning that the program guessed 1 number correctly and it is in the correct position, and guessed 2 other numbers correctly but they are not in the correct position. The Program will continue adjusting its guess with the feedback until it has guessed it correctly.");
+					System.out.println("In the first round the program has no information, so it makes an initial guess, e.g., 1234. Then the FeedbackGenerator will provide a feedback 0A 2B, meaning that the program guessed two digits correctly but they are not in the correct position. So according to this information, in the second round the program will adjust the guess to â€œ2135â€, then the FeedbackGenerator will provide with a feedback â€œ1A 2Bâ€, meaning that the program guessed 1 number correctly and it is in the correct position, and guessed 2 other numbers correctly but they are not in the correct position. The Program will continue adjusting its guess with the feedback until it has guessed it correctly.");
 					System.out.println("*********************************");
 					startGame();
 					break;
@@ -57,37 +51,31 @@ class GuessClass
 					break;
 		}
 	}	
-	void guess()
-	{
+	void guess(){
 		Scanner sc=new Scanner(System.in);		
 		System.out.println("Enter a Four Non-repeated Digit Number, eg.1234,7895:");
 		String usrNo=sc.nextLine();
 		int cc=1;
 		int count=1;
 		String guess="";
-		if(usrNo.length()>4)
-		{
+		if(usrNo.length()>4){
 			System.out.println("Just a Four Digit No. is allowed!");
 			guess();
 		}
-		if(checkRepeat(usrNo)==false)
-		{
+		if(checkRepeat(usrNo)==false){
 			System.out.println("This is not an accepted Four Digit number! Please try again with a different 			number");
 			guess();
 		}
 		
 		else{
-		while(true)
-		{
+		while(true){
 			guess=numberGenerator();	
-			if(guess.isEmpty())
-			{
+			if(guess.isEmpty()){
 				System.out.println("Incorrect Input...Please try Again!!!");
 				return;
 			}
 			
-			if(scoreReturn(guess,result,usrNo))
-			{
+			if(scoreReturn(guess,result,usrNo)){
 				System.out.println("Program: My Guess is: "+guess);
 				System.out.println("Player 2: Smart Guy!!! That is the number");
 				System.out.println("Congratulations!!!");
@@ -109,40 +97,31 @@ class GuessClass
 	}
 }
 	
-	String numberGenerator()
-	{
-		if(genrtdNos.isEmpty())
-		{
+	String numberGenerator(){
+		if(genrtdNos.isEmpty()){
 			return "";
 		}
 		int index=generateRandomNumber(0,genrtdNos.size());
 		String num=genrtdNos.get(index);
 		return num;
 	}
-	boolean scoreReturn(String guess,FeedbackGenerator result,String usrNo)
-	{	
+	boolean scoreReturn(String guess,FeedbackGenerator result,String usrNo){	
 		scoreGenerator(guess,usrNo,result);
 		return (result.getA()==4);
 	}
 	
-	void scoreGenerator(String guess,String fdbk,FeedbackGenerator pr)
-	{
+	void scoreGenerator(String guess,String fdbk,FeedbackGenerator pr){
 		int b=0,c=0;
 		pr.setA(b);
 		pr.setB(c);
-		for (int x=0;x<4;x++)
-		{
-			if(guess.charAt(x)==fdbk.charAt(x))
-			{
+		for (int x=0;x<4;x++){
+			if(guess.charAt(x)==fdbk.charAt(x)){
 				b++;
 				pr.setA(b);
 			}
-			else
-			{
-				for(int y=0;y<4;y++)
-				{
-					if(guess.charAt(x)==fdbk.charAt(y))
-					{
+			else{
+				for(int y=0;y<4;y++){
+					if(guess.charAt(x)==fdbk.charAt(y)){
 						c++;
 						pr.setB(c);
 					}
@@ -151,30 +130,23 @@ class GuessClass
 		}
 	}
 	
-	void numGen()
-	{
-		for(int x=1023;x<9877;x++)
-		{
+	void numGen(){
+		for(int x=1023;x<9877;x++){
 			StringBuffer sb=new StringBuffer("");
 			sb.append(x);
-			if(checkRepeat(sb.toString()))
-			{
+			if(checkRepeat(sb.toString())){
 				genrtdNos.add(sb.toString());
 			}
 		}
 	}
 	
-	boolean checkRepeat(String s)
-	{
+	boolean checkRepeat(String s){
 		int i=0;
-		if(s.charAt(0)=='0')
-		{
+		if(s.charAt(0)=='0'){
 			return false;
 		}
-		while(i<4)
-		{
-			for(int j=i+1;j<4;j++)
-			{
+		while(i<4){
+			for(int j=i+1;j<4;j++){
 				if(s.charAt(i)==(s.charAt(j)))
 					return false;
 			}
@@ -183,58 +155,47 @@ class GuessClass
 		return true;
 	}
 	
-	void removeGenNos(String guess,FeedbackGenerator r)
-	{
+	void removeGenNos(String guess,FeedbackGenerator r){
 		genrtdNos.remove(guess);
 		Iterator<String> pi = genrtdNos.iterator();
-		while(pi.hasNext())
-		{
-			if(checkRepeatIterations(guess,pi.next(),r))
-			{
+		while(pi.hasNext()){
+			if(checkRepeatIterations(guess,pi.next(),r)){
 				pi.remove();
 			}
 		}		
 				
 	}
 	
-	boolean checkRepeatIterations(String guess,String ts,FeedbackGenerator result)
-	{
+	boolean checkRepeatIterations(String guess,String ts,FeedbackGenerator result){
 		scoreGenerator(guess,ts,fdbk);
 		return(fdbk.getA()!=result.getA() || fdbk.getB()!=result.getB());
 	}
 	
-	int generateRandomNumber(int min,int max)
-	{
+	int generateRandomNumber(int min,int max){
 		int range = (max - min);     
    		return (int)(Math.random() * range) + min;
 	}
 }
-class FeedbackGenerator
-{
+class FeedbackGenerator{
 	int A;
 	int B;
 	public FeedbackGenerator()
 	{}
 	
-	public FeedbackGenerator(int A,int B)
-	{
+	public FeedbackGenerator(int A,int B){
 		this.A=A;
 		this.B=B;
 	}
-	public int getA()
-	{
+	public int getA(){
 		return A;
 	}
-	public int getB()
-	{
+	public int getB(){
 		return B;
 	}
-	public void setA(int A)
-	{
+	public void setA(int A){
 		this.A=A;
 	}
-	public void setB(int B)
-	{
+	public void setB(int B){
 		this.B=B;
 	}
 }
